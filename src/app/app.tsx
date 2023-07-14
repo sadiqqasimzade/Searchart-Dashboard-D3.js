ae980d4e234e6206a609517267293b7cc825ada7
sadiq <sadiq_2001@mail.ru>
2023-07-14 15:15:23 +0400

    final wothout context

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { AppContext, AppContextProvider } from 'src/contexts/appContext';
 import Filters from '../components/filters';
 export function App() {
   return (
-    // <AppContextProvider>
       <div className='grid justify-items-center bg-gray-900 min-h-screen'>
         <div className='p-5 w-10/12 text-white rounded-xl'>
           <p className="text-center font-bold text-3xl">Country Comparison</p>
           <Filters />
         </div>
       </div>
-    // </AppContextProvider>
   );
 }
 
